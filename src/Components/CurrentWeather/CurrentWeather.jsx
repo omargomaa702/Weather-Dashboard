e9293c76ae5c62c886d20b792c86708a6e518109
omargomaa702 <omar9980119@gmail.com>
2026-09-09 11:23:27 +0300

    Configure GitHub Pages deployment

--- src/Components/CurrentWeather/CurrentWeather.jsx
+++ src/Components/CurrentWeather/CurrentWeather.jsx
@@ -1,43 +1,49 @@
-import './CurrentWeather.css'
+import "./CurrentWeather.css";
 
-const currentWeather = ({data}) => {
+const currentWeather = ({ data }) => {
   return (
-    <div className='weather'>
+    <div className="weather">
       <div className="top">
         <div>
           <p className="city">{data.city}</p>
           <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img src={`public/icons/${data.weather[0].icon}.png`} alt="weather-icon" className="weather-icon" />
+        <img
+          src={`${import.meta.env.BASE_URL}icons/${data.weather[0].icon}.png`}
+          alt="weather-icon"
+          className="weather-icon"
+        />
       </div>
 
       <div className="bottom">
         <p className="temerature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
-          <div className='parameter-row'>
+          <div className="parameter-row">
             <span className="parameter-label">details</span>
           </div>
 
-          <div className='parameter-row'>
+          <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
 
-          <div className='parameter-row'>
+          <div className="parameter-row">
             <span className="parameter-label">Wind</span>
             <span className="parameter-value">{data.wind.speed}</span>
           </div>
-          <div className='parameter-row'>
+          <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
             <span className="parameter-value">{data.main.humidity}%</span>
           </div>
-          <div className='parameter-row'>
+          <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
             <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default currentWeather;
