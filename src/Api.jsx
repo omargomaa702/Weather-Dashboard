5336bc823997f90e41fce5a430ec7df408013946
omargomaa702 <omar9980119@gmail.com>
2026-09-09 10:46:06 +0300

    change the name of the file

---
+++ src/Api.jsx
@@ -0,0 +1,14 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "9ad1336a393d8da13d0d523979ee1570";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "0268f264acmsh0c287949eb6b4b0p108b56jsnfd1ef9dacd6d",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+    "Content-Type": "application/json",
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import Search from './Components/Search'
+import CurrentWeather from './Components/CurrentWeather/CurrentWeather';
+import {
+  WEATHER_API_URL, WEATHER_API_KEY
+} from './Api';
+import './App.css';
+import { useState } from 'react';
+import Forecast from './Components/Forecast/Forecast';
+
+
+export default function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(' ');
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    const forecastfetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    Promise.all([currentWeatherFetch, forecastfetch]).then(async (Response) => {
+      const weatherResponse = await Response[0].json();
+      const forecastResponse = await Response[1].json();
+
+      setCurrentWeather({
+        city: searchData.label,
+        ...weatherResponse
+      });
+      setForecast({
+        city: searchData.label,
+        ...forecastResponse
+      })
+    }).catch(err => (console.log(err)));
+  }
+
+  console.log(forecast);
+  return (
+    <div className='container'>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  )
+}
